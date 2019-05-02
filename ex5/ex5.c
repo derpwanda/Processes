6fe6334d2ce61ab522441518c473fc6f112d31b2
Wanda <doubleuevans@gmail.com>
2019-05-02 17:15:52 -0500

    explanation of buffer/pipe relationship, ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -42,6 +42,15 @@ int main(void)
         close(fd[1]);
         wait(NULL);
         printf("parent\n");
+        //we are making multiple read calls here
+        //each read reads the MSGSIZE
+        //because there are 3 messages, there will be 3 reads
+
+        //all 3 message are written to the PIPE
+        //the PIPE releases them one at a time
+        //at any one time BUFFER is only holding one message
+        //they are dequeue from the pipe to the buffer. 
+        //when the pipe is empty, the buffer empties, therefore there is nothing left to read.
         while(read(fd[0], buffer, sizeof buffer) != 0){
             printf("%s\n", buffer);
         }
