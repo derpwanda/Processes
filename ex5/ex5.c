d4f01d0abb04fbd7857d7c09e670c6e6a4fec8c4
Wanda <doubleuevans@gmail.com>
2019-05-02 13:08:38 -0500

    still only writes first message

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,6 +17,36 @@ char* msg3 = "hello world #3";
 int main(void)
 {
     // Your code here
-    
+    //fork a child process
+    char buffer[MSGSIZE];
+    int fd[2];
+
+    //pipe(pid); //pipe the fork
+
+    // if (pipe(fd) < 0)
+    // {
+    //     printf("fork failed!");
+    //     exit(1);
+
+    int ex5 = fork();
+    if (ex5 < 0) {
+        printf("fork failed\n");
+        exit(1);
+
+    } else if (ex5 == 0){
+
+        printf("child\n");
+        int written = write(fd[1], msg1, MSGSIZE);
+        int written = write(fd[1], msg2, MSGSIZE);
+        int written = write(fd[1], msg3, MSGSIZE);
+
+    } else {
+
+        printf("parent\n");
+        int read_it = read(0, buffer, sizeof buffer);
+
+        write(STDOUT_FILENO, buffer, read_it);
+    }
+       
     return 0;
 }
